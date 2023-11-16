f247cf107c299a8486f83c7371905407c34e6cbf
vlad <ecscalibur_10@mail.ru>
2023-11-17 00:18:16 +0300

    labs

---
+++ task_2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int rightPart(int n){
+    int k = 1;
+    while (n % 2 != 1){
+        k *= 2;
+        n /= 2;
+    }
+    return k;
+}
+
+
+int rightPartsSum(int a, int b){
+    int sum = 0;
+    for (int i = a; i <= b; ++i)
+        sum += rightPart(i);
+    return sum;
+}
+
+
+int main(void) {
+    system("chcp 65001");
+
+    char *path = (char *) malloc(100 * sizeof(char));
+    scanf("%s", path);
+    realloc(path, strlen(path));
+
+    FILE *in = fopen(path, "r");
+    int a, b;
+    fscanf(in, "%d", &a);
+    fscanf(in, "%d", &b);
+    fclose(in);
+
+    printf("%d", rightPartsSum(a, b));
+
+    return 0;
+}
---
+++ task_4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+int is_prime(int n){
+    for (int i = 2; i < (int) sqrt(n) + 1; ++i) {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+
+int main(void) {
+    system("chcp 65001");
+
+    char *path = (char *) malloc(100 * sizeof(char));
+    scanf("%s", path);
+    realloc(path, strlen(path));
+
+    FILE *in = fopen(path, "r");
+    int n;
+    fscanf(in, "%d", &n);
+    int primes = 0;
+    for (int i = n + 1; i < 2 * n; ++i) {
+        if (is_prime(i)) primes++;
+    }
+    fclose(in);
+
+    FILE *out = fopen("primes_count", "w");
+    fprintf(out, "%d", primes);
+    fclose(out);
+
+    return 0;
+}
